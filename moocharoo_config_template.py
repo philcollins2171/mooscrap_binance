3a2d475cde377e3eaa2f4294776963807ac4629e
philcollins2171 <francoispina@free.fr>
2020-10-11 17:33:31 +0200

    BTC amount no more used

--- moocharoo_config_template.py
+++ moocharoo_config_template.py
@@ -5,7 +5,6 @@
 class config(object):
     username = "your Moocharoo Usr"
     password = "Your Moocharoo pass"
-    capital = "0.26" #100% of this BTC value will be used
     secretkey="Your Binance API"
-    apikey="Your Binance API"
+    apikey="Your Binance API key"
 
